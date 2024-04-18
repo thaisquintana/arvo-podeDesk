f57ea99da5ff2c3d35889560297eea95cdb63604
Thais Quintana <thaisquintanap@gmail.com>
2024-04-17 21:20:19 -0300

    added searchBar component

---
+++ src/app/components/searchBar/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { useState } from "react";
+
+export default function SearchBar() {
+  const [pokemon, setPokemon] = useState<string>("");
+
+  const onSubmit = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      console.log(pokemon);
+    }
+  };
+  return (
+    <div className="flex items-center relative">
+      <input
+        type="text"
+        placeholder="Search"
+        className="rounded-lg w-full p-3 border	border-gray-500 bg-transparent text-gray-700 placeholder-gray-700 focus:border-blue-500 focus:bg-gray-200 focus-visible:outline-none"
+        onKeyDown={onSubmit}
+        onChange={(e) => setPokemon(e.target.value)}
+      />
+      <Search className="absolute right-4 text-gray-700" />
+    </div>
+  );
+}
