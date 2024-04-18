4169fda1bebc2c12b1ed6d8f4ed2e0279e4019cb
Thais Quintana <thaisquintanap@gmail.com>
2024-04-17 21:19:21 -0300

    added header

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+      <header className="w-full px-8 bg-white h-16 flex items-center shadow-md">
+        <div>
+          <p className="text-gray-500 font-semibold">PokeDex!</p>
+        </div>
+      </header>
+    );
+  }
+  
