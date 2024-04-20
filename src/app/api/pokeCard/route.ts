266409d9dd7516307d4744af85075b45252f1b81
Thais Quintana <thaisquintanap@gmail.com>
2024-04-20 01:25:42 -0300

    added api route

---
+++ src/app/api/pokeCard/route.ts
@@ -0,0 +1,21 @@
+// get pokemon list
+export async function getPokemonList(){
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const pokemonList = await response.json()
+    return pokemonList
+}
+
+// get pokemon details
+export async function getPokemonDetails(pokemon: string){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const pokemonDetalis = await response.json()
+    const types = pokemonDetalis.types.map((item: any) => item.type.name)
+    const payload = {
+        id: pokemonDetalis.id, 
+        name: pokemonDetalis.name, 
+        image: pokemonDetalis.sprites.other.dream_world.front_default,
+        categories: types
+    }
+
+    return payload
+}
