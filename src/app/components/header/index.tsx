c6a7d563025de13b3b9896e4c568c51a31366539
Thais Quintana <thaisquintanap@gmail.com>
2024-04-22 01:34:26 -0300

    ident files and change metadata text

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,10 +1,9 @@
 export default function Header() {
-    return (
-      <header className="w-full px-8 bg-white h-16 flex items-center shadow-md">
-        <div>
-          <p className="text-gray-500 font-semibold">PokeDex!</p>
-        </div>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="w-full px-8 bg-white h-16 flex items-center shadow-md">
+      <div>
+        <p className="text-gray-500 font-semibold">PokeDex!</p>
+      </div>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,19 @@ import Card from "./components/card";
 import Header from "./components/header";
 import SearchBar from "./components/searchBar";
 
-
 export default function Home({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | undefined };
-}){
-  const selected = searchParams['pokemon'] ?? '';
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || "";
   return (
     <>
       <Header />
-        <main className="p-8 mx-auto my-0">
-          <SearchBar />
-          <Card data={selected} />
-        </main>
+      <main className="p-8 mx-auto my-0">
+        <SearchBar />
+        <Card query={query} />
+      </main>
     </>
   );
 }
